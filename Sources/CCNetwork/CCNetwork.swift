57ccf1f39ebbddcc8544f9c229a913a4d4cfe552
Rajkumar Singh <devarj1414@gmail.com>
2022-12-22 14:18:56 -0500

    Initial Commit

---
+++ Sources/CCNetwork/CCNetwork.swift
@@ -0,0 +1,6 @@
+public struct CCNetwork {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CCNetworkTests/CCNetworkTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CCNetwork
+
+final class CCNetworkTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CCNetwork().text, "Hello, World!")
+    }
+}
