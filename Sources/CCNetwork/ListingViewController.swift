b877f5d7c4f85a16b8c637d97654ac2571ec6162
Rajkumar Singh <devarj1414@gmail.com>
2022-12-22 20:16:14 -0500

    Added StoryBoard and View Controller

---
+++ Sources/CCNetwork/ListingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListingViewController.swift
+//  
+//
+//  Created by Suruchi Kumari on 12/22/22.
+//
+
+import UIKit
+
+public class ListingViewController: UIViewController {
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
